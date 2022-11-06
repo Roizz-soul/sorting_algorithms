24c72619e29ac4b81f8aac605f8f566f09b07708
Oluremi Abraham <olurmeiabraham9@gmail.com>
2022-11-06 09:55:49 +0000

    Try

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,6 +9,9 @@ void bubble_sort(int *array, size_t size)
 	int temp;
 	size_t i, j;
 
+	if (array == NULL || size == 0 || size == 1)
+		return;
+
 	for (i = 0; i < (size - 1); i++)
 	{
 		for (j = 0; j < (size - 1); j++)
